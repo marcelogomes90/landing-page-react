2d3350eeda81b564107278e4de5b0e2e967d0d36
Marcelo <marcelo.sobrinho@outlook.com>
2022-06-10 15:20:30 -0300

    gallery component created

--- src/components/gallery/index.jsx
+++ src/components/gallery/index.jsx
@@ -4,7 +4,19 @@ function Galeria() {
   
   return (
     <div className="container-galeria">
-      
+      <h1 className='galeria-titulo'>GALERIA</h1>
+      <h1 className='line'></h1>
+      <div className='galeria-box'>
+        <div className='img-galeria' id='gal1'>Lorem Ipsum</div>
+        <div className='img-galeria' id='gal2'>Lorem Ipsum</div>
+        <div className='img-galeria' id='gal3'>Lorem Ipsum</div>
+        <div className='img-galeria' id='gal4'>Lorem Ipsum</div>
+        <div className='img-galeria' id='gal5'>Lorem Ipsum</div>
+        <div className='img-galeria' id='gal6'>Lorem Ipsum</div>
+        <div className='img-galeria' id='gal7'>Lorem Ipsum</div>
+        <div className='img-galeria' id='gal8'>Lorem Ipsum</div>
+        <div className='img-galeria' id='gal9'>Lorem Ipsum</div>
+      </div>
     </div>
   )
 }
